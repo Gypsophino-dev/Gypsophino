77a6538f9512b513e78a3e8628ba16f6c12e8ba8
tonyfettes <feihaoxiang2014@gmail.com>
2020-07-19 01:44:57 +0800

    Add container class

--- src/basic.cpp
+++ src/basic.cpp
@@ -1,12 +1,55 @@
 #include "basic.hpp"
+#include <memory>
+#include <utility>
 
 namespace gyp {
-  statue::statue() {}
-  statue::~statue() {}
-  sprite::sprite() {}
-  sprite::~sprite() {}
-  void sprite::update(int t) {
-    pos.first += t * velocity.first;
-    pos.second += t * velocity.second;
-  }
+statue::statue() : width(-1), height(-1) {}
+statue::~statue() = default;
+sprite::sprite() = default;
+sprite::~sprite() = default;
+void sprite::update(int t) {
+  pos.first += t * velocity.first;
+  pos.second += t * velocity.second;
 }
+
+container::container() {
+  height = -1;
+  width = -1;
+  pos.first = -1;
+  pos.second = -1;
+  split_pos = -1;
+  is_horizontal = -1;
+}
+
+container::container(std::pair<int, int> p, int w, int h)
+    : pos(std::move(p)), width(w), height(h), split_pos(-1), is_horizontal(-1) {
+}
+
+container::container(std::pair<int, int> p, int w, int h, int s, int is_h)
+    : pos(std::move(p)), width(w), height(h), split_pos(s),
+      is_horizontal(is_h) {}
+
+container::~container() = default;
+
+int container::split(int split_p, int is_h) {
+  split_pos = split_p;
+  is_horizontal = is_h;
+  if (child[0] == nullptr && child[1] == nullptr) {
+    if (is_h != 0) {
+      child[0] =
+          std::make_unique<container>(pos, width, split_pos - pos.second);
+      child[1] =
+          std::make_unique<container>(std::make_pair(pos.first, split_pos),
+                                      width, pos.first + height - split_pos);
+    } else {
+      child[0] =
+          std::make_unique<container>(pos, split_pos - pos.first, height);
+      child[1] =
+          std::make_unique<container>(std::make_pair(split_pos, pos.second),
+                                      pos.first + width - split_pos, height);
+    }
+    return 0;
+  }
+  return 1;
+}
+} // namespace gyp
--- src/basic.hpp
+++ src/basic.hpp
@@ -1,32 +1,49 @@
-#ifndef _BASIC_HPP_
-#define _BASIC_HPP_
+#pragma once
 
+#include <array>
+#include <memory>
 #include <raylib.h>
-#include <vector>
 #include <string>
 #include <utility>
+#include <vector>
 
 namespace gyp {
-  struct statue {
-    int height;
-    int width;
-    std::pair<int, int> pos;
-    Color color;
-    Image image;
-    std::string name;
-    statue();
-    ~statue();
-  };
-  struct sprite : statue {
-    std::pair<int, int> velocity;
-    sprite();
-    ~sprite();
-    void update(int t);
-  };
-  template <typename _Tp>
-  struct contain : statue {
-    std::vector<_Tp> content;
-  };
-}
+struct statue {
+  int width;
+  int height;
+  std::pair<int, int> pos;
+  Color color;
+  Image image;
+  std::string name;
+  statue();
+  ~statue();
+};
 
-#endif // _BASIC_HPP_
+struct sprite : statue {
+  std::pair<int, int> velocity;
+  sprite();
+  ~sprite();
+  void update(int t);
+};
+
+class container {
+private:
+  std::array<std::unique_ptr<container>, 2> child;
+
+public:
+  std::pair<int, int> pos; // first -> x, second -> y
+  int width;               // Max x
+  int height;              // Max y
+  int split_pos;
+  int is_horizontal;
+  container();
+  container(const container &a);
+  container(container &&a) noexcept;
+  container(std::pair<int, int> p, int w, int h);
+  container(std::pair<int, int> p, int w, int h, int s, int is_h);
+  ~container();
+  container &operator=(const container &a);
+  container &operator=(container &&a);
+  int split(int split_p, int is_h);
+};
+} // namespace gyp
