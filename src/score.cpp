489ca35d52a8403a9186ed54a3066414512eaab7
tonyfettes <feihaoxiang2014@gmail.com>
2020-07-19 01:45:41 +0800

    Add score::draw()

--- src/score.cpp
+++ src/score.cpp
@@ -1,4 +1,46 @@
 #include "score.hpp"
 
 namespace gyp {
+
+// class parabolic_func:
+parabolic_func::parabolic_func(int hit_time) : hit_time(hit_time) {}
+
+double parabolic_func::operator()(int target_time) {
+  double raw_score = -(target_time - hit_time) * (target_time - hit_time) + 1;
+  return std::max(static_cast<double>(0), raw_score);
 }
+
+// class score:
+template <typename Calc> score<Calc>::score() {
+  letter_grade_table[0] = std::make_pair(std::string("D"), 70);
+  letter_grade_table[1] = std::make_pair(std::string("C"), 80);
+  letter_grade_table[2] = std::make_pair(std::string("B"), 90);
+  letter_grade_table[3] = std::make_pair(std::string("A"), 95);
+  letter_grade_table[4] = std::make_pair(std::string("S"), 99);
+  letter_grade_table[5] = std::make_pair(std::string("SS"), 100);
+  total = GYP_MAX_SCORE;
+  current = 0;
+}
+
+template <typename Calc> score<Calc>::~score() = default;
+
+template <typename Calc>
+void score<Calc>::update(int hit_time, int target_time) {
+  Calc fn;
+  current += fn(hit_time, target_time);
+}
+
+template <typename Calc> std::string score<Calc>::get_letter_grade() {
+  for (int i = 0; i < GRADE_KIND - 1; i++) {
+    if (current < letter_grade_table.at(i).second) {
+      return letter_grade_table.at(i).first;
+    }
+  }
+  return letter_grade_table[GRADE_KIND - 1].first;
+}
+
+template <typename Calc>
+void score<Calc>::draw(const std::pair<int, int>& pos, int font_size) {
+  DrawText(std::to_string(current).c_str(), pos.first, pos.second, font_size, BLUE);
+}
+} // namespace gyp
--- src/score.hpp
+++ src/score.hpp
@@ -1,25 +1,43 @@
-#ifndef _SCORE_HPP_
-#define _SCORE_HPP_
+#pragma once
 
 #include "basic.hpp"
-#include <vector>
+#include <array>
 #include <string>
 
 namespace gyp {
-  struct letter_grade : statue {
-    std::string letter;
-  };
-  struct character : statue {
-    char c;
-  };
-  class score {
-    private:
-      std::vector<std::string> letter_grade_table;
-    public:
-      int total;
-      int current;
-      std::string letter_grade;
-  };
-}
+const int GRADE_KIND = 6;
+const int GYP_MAX_SCORE = 1000000;
+struct letter_grade : statue {
+  std::string letter;
+};
 
-#endif // _SCORE_HPP_
+struct character : statue {
+  char c;
+};
+
+struct parabolic_func {
+private:
+  int hit_time;
+
+public:
+  explicit parabolic_func(int hit_time);
+  double operator()(int target_time);
+};
+
+template <typename CalcFunc = parabolic_func> class score {
+private:
+  std::array<std::pair<std::string, double>, GRADE_KIND> letter_grade_table;
+  std::string letter_grade;
+
+public:
+  int total;
+  double current;
+  score();
+  ~score();
+  void update(int hit_time, int target_time);
+  std::string get_letter_grade();
+  void draw(const std::pair<int, int>& pos, int font_size);
+  void draw(const std::pair<int, int>& pos, int width, int height);
+};
+
+} // namespace gyp
