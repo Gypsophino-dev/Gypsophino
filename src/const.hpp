04b9eeab2afc24765da5c6bbcc05b4178d0997aa
tonyfettes <feihaoxiang2014@gmail.com>
2020-07-19 22:43:57 +0800

    Some constant

---
+++ src/const.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <raylib.h>
+
+namespace gyp {
+const int DEFAULT_WIDTH = 1920;
+const int DEFAULT_HEIGH = 1080;
+const int DEFAULT_TRACK_NUM = 4;
+const Color DEFAULT_FG = BLACK;
+const Color DEFAULT_BG = WHITE;
+const int DEFAULT_GEOMETRY = -1;
+const int DEFAULT_THICKNESS = 5;
+const int DEFAULT_SPEED = 1;
+const int DEFAULT_FONT_SIZE = 20;
+} // namespace gyp
